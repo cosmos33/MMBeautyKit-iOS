a25e88a706b87accf1a5c57dd2878d7b74c7957d
sunfei <sun.fei@immomo.com>
2020-10-20 10:36:45 +0800

    [bugfix] 修复beauty模块不同权限下判断不正确bug

---
+++ MMBeautyKit Interact/Product/MMBeautyKit.framework/PrivateHeaders/MMHandGestureDetectorContext.h
@@ -0,0 +1,23 @@
+//
+//  MMHandGestureDetectorContext.h
+//  CXBeautyKit
+//
+//  Created by sunfei on 2018/11/27.
+//
+
+#import <Foundation/Foundation.h>
+#import "MMImageDetectorProtocol.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMHandGestureDetectorContext : NSObject <MMImageDetectorProtocol>
+
+@property (nonatomic, readonly) id<MMHandGestureResultProtocol> result;
+
+@end
+
+@interface MMLocalHandGestureDetectorContext : MMHandGestureDetectorContext
+
+@end
+
+NS_ASSUME_NONNULL_END
